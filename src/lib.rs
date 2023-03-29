1c06c60fefb01c220af488662111995d3d988086
HyperCodec <72839119+HyperCodec@users.noreply.github.com>
2023-03-28 20:33:13 -0400

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,60 @@
+//! A small crate for weighed choosing from a set.
+
+/// Contains all of the important things from this crate.
+/// When using the crate, you want to do `use choose_rand::prelude::*;`
+pub mod prelude;
+
+use std::fmt;
+
+/// Simple Error struct that has a String value for whatever reason it errored.
+#[derive(Debug, Clone)]
+pub struct Error(String);
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Error: {}", self.0)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashSet;
+
+    use super::prelude::*;
+
+    #[derive(Hash, Eq, PartialEq, Clone, Debug)]
+    enum Thing {
+        A,
+        B,
+        C,
+        D,
+    }
+
+    impl Probable for Thing {
+        fn probability(&self) -> f64 {
+            match self {
+                Thing::A => 0.15,
+                Thing::B => 0.35,
+                Thing::C => 0.45,
+                Thing::D => 0.05,
+            }
+        }
+    }
+
+    #[test]
+    fn test() {
+        let mut selection = HashSet::new();
+
+        {
+            let original = [Thing::A, Thing::B, Thing::C, Thing::D];
+
+            for o in original {
+                selection.insert(o);
+            }
+        }
+
+        let chosen = choose_rand(&selection).unwrap();
+
+        println!("The chosen one is: {:?}", chosen);
+    }
+}
---
+++ src/prelude.rs
@@ -0,0 +1,47 @@
+use std::collections::{HashSet, BTreeSet};
+use crate::Error;
+
+/// Marker trait used by `choose_rand`.
+/// Allows function to accept either type of set.
+pub trait Set: IntoIterator<Item = <Self as Set>::Item> {
+    type Item;
+}
+
+impl<'a, P: Probable> Set for &'a HashSet<P> {
+    type Item = &'a P;
+}
+impl<'a, P: Probable> Set for &'a BTreeSet<P> {
+    type Item = &'a P;
+}
+
+/// Required for `chooe_rand` to work.
+/// Use on any items to be chosen.
+pub trait Probable: Clone {
+    fn probability(&self) -> f64;
+}
+
+/// Pick a random item from the set,
+/// weighed by `item.probability()`
+pub fn choose_rand<T, S>(s: &S) -> Result<T, Error>
+where 
+    T: Probable,
+    for<'a> &'a
+    S: Set<Item = &'a T>
+{
+    let r = fastrand::f64();
+
+    let mut last = 0.;
+    for choice in s {
+        let p = choice.probability();
+
+        let newlast = last + p;
+
+        if (last..newlast).contains(&r) {
+            return Ok(choice.clone());
+        }
+
+        last = newlast;
+    }
+
+    Err(Error("Probabilities must add up to 1".to_string()))
+}
