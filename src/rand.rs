a08d21621289c7886f6f3791589a463f26383171
Tristan Murphy <72839119+HyperCodec@users.noreply.github.com>
2023-09-08 15:39:45 +0000

    implement refcell system to avoid clone

--- src/rand.rs
+++ src/rand.rs
@@ -1,13 +1,14 @@
 use weighted_rand::builder::*;
 use rand::Rng;
+use std::cell::RefCell;
 
 /// Marker trait used by `choose_rand`.
 /// Allows function to accept either type of set.
-pub trait ProbabilityGroup: IntoIterator<Item = <Self as ProbabilityGroup>::Item> + std::ops::Index {
+pub trait ProbabilityGroup: IntoIterator<Item = RefCell<<Self as ProbabilityGroup>::Item>> + std::ops::Index {
     /// The type of item in the Set
     type Item;
 
-    fn choose_rand<T>(&self, rng: &mut impl Rng) -> Result<T>
+    fn choose_rand(&self, rng: &mut impl Rng) -> Result<Ref<Self::Item>>
     where
         T: Probable + Clone, // TODO remove clone functionality and do it with refcell or something
     {
@@ -24,7 +25,27 @@ pub trait ProbabilityGroup: IntoIterator<Item = <Self as ProbabilityGroup>::Item
         
         let i = wt.next_rng(rng);
 
-        self[i].clone()
+        Ok(self[i].borrow())
+    }
+
+    fn choose_rand_mut(&mut self, rng: &mut impl Rng) -> Result<RefMut<Self::Item>>
+    where
+        T: Probable + Clone, // TODO remove clone functionality and do it with refcell or something
+    {
+        let weights: Vec<f64> = self.into_iter()
+            .map(|p| p.probability())
+            .collect();
+
+        if weights.iter().sum() != 1. {
+            return Err(Error("Individual probabilities must add up to 1"));
+        }
+
+        let wt = WalkerTableBuilder::new(&weights)
+            .build();
+        
+        let i = wt.next_rng(rng);
+
+        Ok(self[i].borrow_mut())
     }
 }
 
@@ -32,7 +53,7 @@ impl<T> ProbabilityGroup for T
 where
     T: IntoIterator<Item = <Self as ProbabilityGroup>::Item> + std::ops::Index
 {
-    
+
 }
 
 /// Required for `chooe_rand` to work.
